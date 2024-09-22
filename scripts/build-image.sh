cf0360c65c5306645750a252396814c9b98ab4fa
Kanwar Ram <kanwarram17@gmail.com>
2024-09-22 12:54:04 +1000

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,9 +4,6 @@ set -u
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-# Use docker-compose to build the images for all services defined in the compose file
-docker-compose build
-
-# Tag each image with the version and push to the container registry
-docker tag book_catalog:latest $CONTAINER_REGISTRY/book_catalog:$VERSION
-docker tag inventory_management:latest $CONTAINER_REGISTRY/inventory_management:$VERSION
+# Build Docker images for book_catalog and inventory_management
+docker build -t $CONTAINER_REGISTRY/book_catalog:$VERSION ./book_catalog
+docker build -t $CONTAINER_REGISTRY/inventory_management:$VERSION ./inventory_management
