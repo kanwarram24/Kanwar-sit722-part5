de807b8c460817ad193b432e61a14898d311eca0
Kanwar Ram <kanwarram17@gmail.com>
2024-09-22 13:08:54 +1000

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,6 +4,11 @@ set -u
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
+# Debugging - check if the variables are being set correctly
+echo "Building images with:"
+echo "CONTAINER_REGISTRY: $CONTAINER_REGISTRY"
+echo "VERSION: $VERSION"
+
 # Build Docker images for book_catalog and inventory_management
 docker build -t $CONTAINER_REGISTRY/book_catalog:$VERSION ./book_catalog
 docker build -t $CONTAINER_REGISTRY/inventory_management:$VERSION ./inventory_management
