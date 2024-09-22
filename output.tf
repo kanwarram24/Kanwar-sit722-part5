1098e2fd3776df39987317b64224a4b9e9085fda
Kanwar Ram <kanwarram17@gmail.com>
2024-09-22 11:01:02 +1000

    updated output file

--- output.tf
+++ output.tf
@@ -1,9 +1,14 @@
+# Outputs the container registry login server URL
 output "container_registry_url" {
-  value = azurerm_container_registry.acr.login_server
+  value = azurerm_container_registry.container_registry.login_server
 }
+
+# Outputs the Kubernetes cluster name
 output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  value = azurerm_kubernetes_cluster.cluster.name
 }
+
+# Outputs the resource group name
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.flixtube.name
 }
