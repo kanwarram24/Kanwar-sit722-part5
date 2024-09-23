046c1c1e4e41717087fadd010da6b6855b017c00
Kanwar Ram <kanwarram17@gmail.com>
2024-09-23 17:30:40 +1000

    Update delete.sh

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,8 @@
-# Your Solution
+#!/bin/bash
+
+set -u
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+# Use kubectl to deploy the services
+envsubst < ./scripts/kubernetes/deployment.yaml | kubectl delete -f -
